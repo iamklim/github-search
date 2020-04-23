d99bb64e4b99f8affa5e098150c3a74603a34d97
etoklim <vlad.klymenko@softermii.com>
2020-04-23 21:05:10 +0300

    Updates

---
+++ precache-manifest.157162d3766e24b8cb2b147868902ed1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127a961a52e4a959945c1997ad54e2d9",
+    "url": "/github-search/index.html"
+  },
+  {
+    "revision": "fc05c4eb7728f8b1bd0e",
+    "url": "/github-search/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "dd1dff3608536537515e",
+    "url": "/github-search/static/css/main.a2a3449f.chunk.css"
+  },
+  {
+    "revision": "fc05c4eb7728f8b1bd0e",
+    "url": "/github-search/static/js/2.16f82791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-search/static/js/2.16f82791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1dff3608536537515e",
+    "url": "/github-search/static/js/main.1a428cf0.chunk.js"
+  },
+  {
+    "revision": "9adcc22a38b87a4fbfc3",
+    "url": "/github-search/static/js/runtime-main.db05b65a.js"
+  }
+]);
